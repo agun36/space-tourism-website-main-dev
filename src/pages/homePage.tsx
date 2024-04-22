d3158a7353679d70c7d486160b99f0351a0180fc
agun36 <aguntoroti10@gmail.com>
2024-04-22 17:22:12 +0100

    modified homepage

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -5,12 +5,12 @@ import { Navigation } from '../components/navigation';
 export const HomePage = () => {
 
     return (
-        <div className='home  d-flex flex-column  px-md-4 px-sm-0 min-vh-100 max-vh-auto'>
-            <div className='mt-md-5'>
+        <div className='home row   px-md-4 px-sm-0 min-vh-100 max-vh-auto'>
+            <div className='mt-md-5 col-12'>
                 <Navigation />
             </div>
-            <div className='m-auto row align-items-center'>
-                <div className='home-text__header mt-5  col-md-5'>
+            <div className='col-12 container  row justify-content-center align-items-center'>
+                <div className='home-text__header   col-md-6 mx-auto text-sm-center'>
                     <h1 className='text-texts-color'>
                         so, you want to tarvel to <br /> <small className='text-white-000'>
                             space
@@ -20,7 +20,7 @@ export const HomePage = () => {
                         Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
                     </p>
                 </div>
-                <p className='home-link__wrapper mt-7  mt-md-0 ms-md-auto  col-md-4'>
+                <p className='home-link__wrapper     col-md-6'>
                     <NavLink className=' home-link__explore ' to='/destination'>
                         Explore
                     </NavLink>
