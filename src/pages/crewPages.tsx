6fdf5bfb2b9e4606d8f2f76d0838665660fc6d83
agun36 <aguntoroti10@gmail.com>
2024-04-24 08:35:12 +0100

    modified homepage vairables styles crewpages technology

--- src/pages/crewPages.tsx
+++ src/pages/crewPages.tsx
@@ -11,17 +11,17 @@ export const CrewPages = () => {
     };
 
     return (
-        <div className='crew row  px-md-4 px-sm-0 min-vh-100 max-vh-auto'>
+        <div className='crew row row-cols-lg-2  px-md-4 px-sm-0 min-vh-100'>
 
-            <div className='mt-sm-0 mt-md-5 px-4 col-12'>
+            <div className='mt-sm-0 mt-md-5 px-4 col-12 col-lg-12'>
                 <Navigation />
             </div>
-            <div className="col-12 row align-items-center justify-content-md-center  mx-auto">
+            <div className="col-12 col-lg-12 row row-cols-lg-1    flex-lg-column align-items-center justify-content-md-center   mx-auto">
                 <div className='mt-8 mt-md-8 col-12 d-flex justify-content-center justify-content-sm-start justify-content-md-start px-4'>
                     <h1 className='text-white-000 d-flex gap-3 text-uppercase'><span className='opacity-50'>00</span>meet your crew</h1>
                 </div>
-                <div className="mt-5 about-crew col-12   row align-items-center justify-content-md-center  container">
-                    <div className='about-crew__text  text-center text-md-start col-md-5 col-sm-12 row'>
+                <div className="mt-5 about-crew col-12 flex-lg-row row align-items-center justify-content-md-center  container">
+                    <div className='about-crew__text  text-center text-lg-start text-md-start col-md-5 col-sm-12 row'>
                         <div className='mt-md-8 col-12 '>
                             <h1 className='text-white-000 font-size-sm font-size-md'>{currentCrew.role}</h1>
                             <h2 className='text-white-000'>{currentCrew.name}</h2>
@@ -39,7 +39,7 @@ export const CrewPages = () => {
 
                     </div>
                     <div className='col-sm-12 col-md-7 d-flex justify-content-center'>
-                        <img src={currentCrew.images.png} alt="Crew member" />
+                        <img src={currentCrew.images.png} alt="Crew member" className='min-vh-100' />
                     </div>
                 </div>
             </div>
