f9b7bad5e81ae5bf74df46d53271729d66292d68
agun36 <aguntoroti10@gmail.com>
2024-04-24 11:26:44 +0100

    modified styles technology

--- src/pages/technologyPages.tsx
+++ src/pages/technologyPages.tsx
@@ -17,7 +17,7 @@ export const TechnologyPages = () => {
             </div>
             {/* second column */}
             <div className='col-12 col-lg-12 row row-cols-lg-1    flex-lg-colum align-items-center justify-content-md-center   mx-auto my-lg-8'>
-                <div className='text-white-000 my-8   col-12 col-lg-12 d-flex  justify-content-center justify-content-sm-start justify-content-md-start bg-lg-danger p-lg-5'>
+                <div className='text-white-000    col-12 col-lg-12 d-flex  justify-content-center justify-content-sm-start justify-content-md-start bg-lg-danger p-lg-5'>
                     <h1 className='d-flex gap-2'><span className='opacity-50'>03</span>Technology Pages</h1>
                 </div>
                 <div className="about-technology col-12 col-lg-12 flex-lg-row   row row-sm-col-1 align-items-sm-center justify-content-sm-center me-md-auto  ">
